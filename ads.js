a62f1ed2d92f19461143ad534bb75b8428711064
mazn <mazn.dev@gmail.com>
2024-08-27 22:33:40 +0900

    create ads.js

---
+++ ads.js
@@ -0,0 +1,11 @@
+var header_cd        = 'spad0060';
+var in_article_cd    = '';
+var footer_cd        = 'spad0061';
+var overlay_cd       = '';
+var image_header_cd  = 'spad0060';
+var image_overlay_cd = 'spad0061';
+
+const link = document.createElement('link');
+link.rel = 'stylesheet';
+link.href = 'https://raw.githubusercontent.com/mazn/ld_blog/main/ld.css';
+document.head.appendChild(link);
